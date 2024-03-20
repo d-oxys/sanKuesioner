46c7c431f1c456528ba499a0c4ea08f4e5553da3
D-oxys <sanuzumaki12@gmail.com>
2024-03-20 15:38:21 +0700

    fix: teks typo

--- src/components/tutorial.tsx
+++ src/components/tutorial.tsx
@@ -35,7 +35,7 @@ function Dropdown() {
               <div className='flex flex-col items-center sm:flex-row'>
                 <div className='mx-auto flex w-full items-center justify-start'>
                   <div className='w-full sm:w-1/2 sm:pr-8'>
-                    <div className='rounded bg-white p-4 shadow'>lakukan pendaftaran terlebih dahuli agar memiliki akun untuk dapat login ke aplikasi</div>
+                    <div className='rounded bg-white p-4 shadow'>lakukan pendaftaran terlebih dahulu agar memiliki akun untuk dapat login ke aplikasi</div>
                   </div>
                 </div>
                 <div className='absolute left-1/2 flex h-9 w-9 -translate-x-1/2 -translate-y-4 transform items-center justify-center rounded-full border-4 border-white bg-blue-500 sm:translate-y-0'>
